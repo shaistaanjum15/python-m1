ab1c5f690e9195de5c6816b927dca71240188bc2
shaistaanjum15 <shaistaanjum15@gmail.com>
2026-09-20 18:57:37 +0530

    mean value python challenge

--- python_challenge.py
+++ python_challenge.py
@@ -29,3 +29,21 @@ if numn%numd==0:
   print("\n" +str(numn)+ " is divisible by " +str(numd))
 else:
   print("\n" +str(numn)+ " is not divisible by " +str(numd))
+  
+
+#Mean
+mean1 = 38
+wrong_number=36
+correct_number=56
+total_number=40
+#sum of 40 numbers
+sum = mean1*total_number
+print("the sum of 40 number: ",sum)
+
+#correct sum of these numbers
+num2=sum-((wrong_number)-(correct_number))
+print("sum-((wrong_number)-(correct_number)): ",num2)
+
+#the correct mean
+mean2=num2/total_number
+print(mean2)
