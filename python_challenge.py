90f81b1d9e57d8da4e87bfbe9a4e7b754c5f1133
shaistaanjum15 <shaistaanjum15@gmail.com>
2026-09-20 18:50:08 +0530

    is the number divisible or not

--- python_challenge.py
+++ python_challenge.py
@@ -16,3 +16,16 @@ if name == "Alex" or name == "John" and age >= 2 :
   print("Hello! Welcome.")
 else :
   print("Good Bye!!")
+  
+  
+#Divisible number
+
+print("Enter a Number (Numerator): ")
+numn = int(input())
+print("Enter a Number (denominator): ")
+numd = int(input())
+
+if numn%numd==0:
+  print("\n" +str(numn)+ " is divisible by " +str(numd))
+else:
+  print("\n" +str(numn)+ " is not divisible by " +str(numd))
