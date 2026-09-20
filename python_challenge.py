1384bf315035e8323e50cad8ac99b97e10bcba34
shaistaanjum15 <shaistaanjum15@gmail.com>
2026-09-20 18:59:04 +0530

    average speed

--- python_challenge.py
+++ python_challenge.py
@@ -47,3 +47,28 @@ print("sum-((wrong_number)-(correct_number)): ",num2)
 #the correct mean
 mean2=num2/total_number
 print(mean2)
+
+#Average speed
+a = int(input("enter a value: "))
+b = int(input("enter value 2 :"))
+c = int(input("enter value 3: "))
+
+avg = (a + b + c) / 3
+print("avg =", avg)
+
+if avg > a and avg > b and avg > c:
+    print("%d is higher than %d, %d, %d" %(avg, a, b, c))
+elif avg > a and avg > b:
+    print("%d is higher than %d, %d" %(avg, a, b))
+elif avg > a and avg > c:
+    print("%d is higher than %d, %d" %(avg, a, c))
+elif avg > b and avg > c:
+    print("%d is higher than %d, %d" %(avg, b, c))
+elif avg > a:
+    print("%d is just higher than %d" %(avg, a))
+elif avg > b:
+    print("%d is just higher than %d" %(avg, b))
+elif avg > c:
+    print("%d is just higher than %d" %(avg, c))
+else:
+  print("invalid input")
